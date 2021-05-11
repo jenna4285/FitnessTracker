384dffc484a108dff1275ab04a90841d05dfc6a6
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-05-11 16:55:29 -0500

    model created

---
+++ models/workout.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date
+    },
+    exercises: [
+       {
+        type: {
+            type: String,
+        },
+        name: {
+            type: String,
+        }, 
+        duration: { 
+            type: Number,
+        }, 
+        weight: {
+            type: Number,
+        }, 
+        reps: {
+            type: Number,
+        }, 
+        sets: {
+            type: Number,
+       },
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
